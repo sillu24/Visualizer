b843235d836a9faaa7f2983e712580dc71f1928a
Sahil Chawla <sahilchawla24@gmail.com>
2022-11-03 21:20:41 +0530

    first commit

---
+++ src/PathfindingVisualizer/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import './navbar.css';
+export class Navbar extends Component {
+  render() {
+    const{
+      visualizeDijkstra,
+    }=this.props;
+    return (
+        <div className='navbar'>
+            <div className="Nodes1">
+            <div className='StartingNode'>Starting Node</div>
+            <div className='TargetNode'>Target Node</div>
+            </div>
+            <div>
+            <button className='first' onClick={() => visualizeDijkstra()}>Visualize Dijkstra's Algorithm</button>
+            </div>
+            <div>
+            <a href="http://localhost:3000"><button className='first'>Reset Visualizer</button></a>
+            </div>
+            <div className="Nodes2">
+            <div className='UnvisitedNode'>Unvisited Node</div>
+            <div className='VisitedNode'>Visited Nodes</div>
+            <div className='ShortestNode'>Shortest-path Node</div>
+            </div>
+        </div>
+    )
+  }
+}
+
+export default Navbar
